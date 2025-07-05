13ed8cc366eef57ddcb5dda2716435a4c7f448a5
Carl Erosa <carlmelvinerosa3@gmail.com>
2025-07-05 12:12:38 +0800

    fixed bugs and finished landing page

--- weatherwithus/src/app/page.tsx
+++ weatherwithus/src/app/page.tsx
@@ -9,21 +9,48 @@ export default function Home() {
   const router = useRouter();
   
   return (
-    <main>
+    <>
+    <title>Weathering With Us</title>
       <div className={styles.Title}>Weathering with us</div>
       <div className={styles.Subtitle}>
         <h4>Track Fast, Track Now</h4>
       </div>
       <div className={styles.ButtonContainer}>
-        <Button className={styles.Button} variant="secondary"onClick={() => router.push('/tracking')}>
+        <Button className={styles.Button} variant="secondary" onClick={() => router.push('/tracking')}>
           Track Now
         </Button>
       </div>
-        <div className={styles.ButtonContainer2}>
+      <div className={styles.ButtonContainer2}>
         <Button className={styles.Button2} variant="secondary">
           Learn More
         </Button>
       </div>
-    </main>
+      <div className={styles.textContainer}>
+        <h2 className={styles.Text}>90%</h2>
+      </div>
+      <div className={styles.textContainer2}>
+        <h2 className={styles.Text2}>Accuracy</h2>
+      </div>
+      <div className={styles.textContainer3}>
+        <h2 className={styles.Text3}>
+          24/7
+        </h2>
+      </div>
+      <div className={styles.textContainer4}>
+        <h2 className={styles.Text4}>
+          Updates
+        </h2>
+      </div>
+      <div className={styles.textContainer5}>
+        <h2 className={styles.Text5}>
+          Global
+        </h2>
+      </div>
+      <div className={styles.textContainer6}>
+        <h2 className={styles.Text6}>
+          Coverage
+        </h2>
+      </div>
+    </>
   );
 }
--- weatherwithus/src/app/tracking/page.tsx
+++ weatherwithus/src/app/tracking/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./tracking.module.css";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation"
+ 
+ 
  const tracking = () =>{
-    return <div>Hello World</div>;
+    const router = useRouter();
+    return (
+    <div className={styles.TitleContainer}>
+        <h1>Weather Dashboard</h1>
+    </div>
+
+        
+    );
 }
 
 export default tracking;
